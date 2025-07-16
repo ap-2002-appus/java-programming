9534d51579e05348459c4084a90aba2be2ba8480
Ibin Siby <ibinsiby4619@gmail.com>
2025-07-16 09:36:18 +0500

    Add files via upload

---
+++ Checknumber.java
@@ -0,0 +1,20 @@
+
+public class Checknumber {
+	public static void main(String [] args)
+	{
+		int number = 10;
+		if(number>0)
+		{
+			System.out.println("Possitive");
+		}
+		else if(number< 0)
+		{
+			System.out.println("Negative");
+		}
+		else 
+		{
+			System.out.println("Number is zero");
+		}
+	}
+
+}
