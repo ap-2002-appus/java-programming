f343abc8593fdede4604b3853303bbe0baccf05e
Ibin Siby <ibinsiby4619@gmail.com>
2025-07-23 10:02:44 +0530

    Add files via upload

---
+++ odd_num.java
@@ -0,0 +1,14 @@
+package java_lab;
+
+public class odd_num {
+	  public static void main(String[] args) {
+	        for (int i = 1; i <= 50; i++) {
+	        	if (i%2==0) {
+	        		continue;
+	        	}
+	        	else {
+	            System.out.println(i);
+	        	}
+}
+}
+}
