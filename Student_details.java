a2182d27cfa9bf8d5f8491e81941663f17b5451d
Ibin Siby <ibinsiby4619@gmail.com>
2025-07-23 10:36:38 +0530

    Add files via upload

---
+++ Student_details.java
@@ -0,0 +1,22 @@
+package java_lab;
+import java.util.Scanner;
+public class Student_details {
+	String Name;
+	int age;
+public void studentDetails() {
+	System.out.println("Name:"+Name);
+	System.out.println("Roll number:"+age);
+}
+	public static void main(String [] args) {
+	Scanner sc=new Scanner(System.in);
+		Student_details std=new Student_details();
+		System.out.println("Enter name:");
+		std.Name=sc.nextLine();
+		System.out.println("Enter age:");
+		std.age=sc.nextInt();
+		std.studentDetails();
+}
+
+}
+		
+		
