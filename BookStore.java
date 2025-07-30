c5214fff65c3215ee5e19816116255a3b9bb37fc
Ibin Siby <ibinsiby4619@gmail.com>
2025-07-30 10:49:10 +0530

    Add files via upload

---
+++ BookStore.java
@@ -0,0 +1,47 @@
+class Book{
+	String title;
+	String author;
+	double price;
+	int quantity;
+	double totalValue;
+
+Book(String title,String author,double price,int quantity){
+	this.title = title;
+	this.author = author;
+	this.price = price;
+	this.quantity = quantity;
+	
+}
+public void totalVal() {
+	totalValue = price*quantity;
+}
+public void displayDetails() {
+	System.out.println("Title of the Book:"+title);
+	System.out.println("Author:"+author);
+	System.out.println("Price:"+price);
+	System.out.println("Quantity:"+quantity);
+	System.out.println("Total Value:"+totalValue);
+	
+}
+
+}
+
+
+public class BookStore {
+
+	public static void main(String[] args) {
+		Book b1 = new Book("Wings ","ghghg",500,5);
+		b1.totalVal();
+		b1.displayDetails();
+		Book b2 = new Book("world wide","ygj",465,10);
+		b2.totalVal();
+		b2.displayDetails();
+	
+	}
+
+}
+		
+
+	
+
+
