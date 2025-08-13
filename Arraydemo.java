08eef0a5a1ce7f8bec506ce53dd75c1dbc9b7ac9
Ibin Siby <ibinsiby4619@gmail.com>
2025-08-13 09:52:24 +0500

    Add files via upload

---
+++ Arraydemo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Arraydemo {
+	public static void main(String[]args)
+	{
+		
+		int [] array =new int[10];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the elements");
+		for(int i=0;i<10;i++) {
+			array[i]=sc.nextInt();
+		}
+		for(int num:array) {
+			System.out.print(num+"\t");;
+			
+		}
+	}
+
+}
